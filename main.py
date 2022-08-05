b3c8bcf0df8fe8b63747e92e889e949418b88319
chandram13 <mschandr@uci.edu>
2022-08-04 22:09:29 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Marvish Chandra
+
+
+
+def mainCompute(name):
+    customerLotto = []
+    for i = 0, i.length, i++ {
+    for i in range(0,99)
+    return i in range(0,5)
+    return i += customerLotto
+    print("The randomly generated lottery ticket is" + customerLotto + ".")
+    }
